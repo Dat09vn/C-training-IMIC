757da62784c02bce50a009c0dd2514f5ca97e938
Dat <tiendat14031998@gmail.com>
2020-05-06 22:21:05 +0700

    add document 06052020

---
+++ Instructor/Sources/wcscat_ function.cpp
@@ -0,0 +1,31 @@
+// C++ program to demonstrate
+// example of wcscat() function.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int wcscat_ function()
+{
+
+    // maximum length of the destination string
+    wchar_t dest[30];
+
+    // maximum length of the source string
+    wchar_t src[30];
+
+    // initialize the destination string
+    wcscpy(dest, L"IMIC Technology");
+
+    // initialize the source string
+    wcscpy(src, L" is the best");
+
+
+    wcscat(dest, src);
+    wprintf(L"%ls\n", dest);
+    return 0;
+}
+/* Output *//*
+IMIC Technology is the best
+*/
+
+
